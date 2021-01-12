281dddc70d9c4f27080c6e9aade9a5869cafbbdb
tmikma <tmikmal@gmail.com>
2021-01-12 17:47:37 +0700

    jaban conditional nomor 2

--- Tugas-2-String/Conditional.js
+++ Tugas-2-String/Conditional.js
@@ -15,3 +15,24 @@ if (nama == "John"){
 }   else if(nama == ""){
     console.log("Nama Harus di isi")
 }
+console.log("=====================================")
+var hari = 1; 
+var bulan = 5; 
+var tahun = 1945;
+switch(bulan){
+    case 1: bulan = 'Januari'; break;
+    case 2: bulan = 'Februari'; break;
+    case 3: bulan = 'Maret'; break;
+    case 4: bulan = 'April'; break;
+    case 5: bulan = 'Mei'; break;
+    case 6: bulan = 'Juni'; break;
+    case 7: bulan = 'Juli'; break;
+    case 8: bulan = 'Agustus'; break;
+    case 9: bulan = 'September'; break;
+    case 10: bulan = 'Oktober'; break;
+    case 11: bulan = 'November'; break;
+    case 12: bulan = 'Desember'; 
+}
+var tanggal = hari +" "+ bulan +" "+ tahun;
+console.log(tanggal);
+console.log("=====================================")
