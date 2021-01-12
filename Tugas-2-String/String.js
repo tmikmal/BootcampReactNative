0fef6d2e326efb2f22bac8a635d61b6b2298dab0
tmikma <tmikmal@gmail.com>
2021-01-12 12:31:47 +0700

    jawaban nomor 3

--- Tugas-2-String/String.js
+++ Tugas-2-String/String.js
@@ -29,4 +29,14 @@ console.log('seven Word: ' + seventhWord);
 console.log('eight Word: ' + eighthWord);
 
 //jawaban nomor 3
-
+var sentence2 = 'wow JavaScript is so cool';
+var FirstWord2 = sentence2.substring(0, 3); 
+var secondWord2 = sentence2.substring(4, 14);
+var thirdWord2 = sentence2.substring(15, 17);
+var fourthWord2 = sentence2.substring(18, 20); 
+var fifthWord2 = sentence2.substring(21, 25);
+console.log('First Word: ' + FirstWord2); 
+console.log('Second Word: ' + secondWord2);
+console.log('Third Word: ' + thirdWord2);
+console.log('Fourth Word: ' + fourthWord2); 
+console.log('Fifth Word: ' + fifthWord2);
