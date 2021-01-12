c18c0b6b534028d8eb3a152904146456ecef30e9
tmikma <tmikmal@gmail.com>
2021-01-12 12:24:32 +0700

    jawaban no 2

---
+++ Tugas-2-String/Conditional.js
@@ -0,0 +1,15 @@
+var sentence = "I am going to be React Native Developer";
+
+var firstWord = sentence[0] ; 
+var secondWord =  sentence[2] + sentence[3]  ;
+var thirdWord = sentence[5] + sentence[6] + sentence[7] + sentence[8] + sentence[9] ;
+var fourthWord = sentence[11] + sentence[12] ; 
+var fifthWord; // lakukan sendiri 
+var sixthWord; // lakukan sendiri 
+var seventhWord; // lakukan sendiri 
+var eighthWord; // lakukan sendiri 
+
+console.log('First Word: ' + firstWord);
+console.log('Second Word: ' + secondWord);
+console.log('Third Word: ' + thirdWord);
+console.log('Fourt Word: ' + fourthWord);
---
+++ Tugas-2-String/String.js
@@ -0,0 +1,32 @@
+//Jawaban No.1
+var word = 'JavaScript '; 
+var second = 'is '; 
+var third = 'awesome '; 
+var fourth = 'and '; 
+var fifth = 'I '; 
+var sixth = 'love '; 
+var seventh = 'it! ';
+console.log(word + second + third + fifth + sixth + seventh)
+
+//Jawaban No.2
+var sentence = "I am going to be React Native Developer";
+var firstWord = sentence[0] ; 
+var secondWord =  sentence[2] + sentence[3]  ;
+var thirdWord = sentence[5] + sentence[6] + sentence[7] + sentence[8] + sentence[9] ;
+var fourthWord = sentence[11] + sentence[12] ; 
+var fifthWord = sentence[14] + sentence[15] ;
+var sixthWord =  sentence[17] + sentence[18] + sentence[19] + sentence[20] + sentence[21] ;
+var seventhWord = sentence[23] + sentence[24] + sentence[25] + sentence[26] + sentence[27] + sentence[28]; 
+var eighthWord = sentence[30] + sentence[31] + sentence[32] + sentence[33] + sentence[34] + sentence[35] + sentence[36] + sentence[37] + sentence[38];
+
+console.log('First Word: ' + firstWord);
+console.log('Second Word: ' + secondWord);
+console.log('Third Word: ' + thirdWord);
+console.log('Fourt Word: ' + fourthWord);
+console.log('fifth Word: ' + fifthWord);
+console.log('six Word: ' + sixthWord);
+console.log('seven Word: ' + seventhWord);
+console.log('eight Word: ' + eighthWord);
+
+//jawaban nomor 3
+
