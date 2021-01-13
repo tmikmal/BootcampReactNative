5c462cbed6cc39b4c59271e6db79ac7920f0e0e1
tmikma <tmikmal@gmail.com>
2021-01-13 14:00:37 +0700

    jawaban soal nomor 2

--- Tugas-3-Loop/looping.js
+++ Tugas-3-Loop/looping.js
@@ -1,3 +1,4 @@
+console.log("=====================================")
 var pertama;
 var kedua;
 console.log('Looping Pertama');
@@ -7,4 +8,18 @@ console.log('Looping Pertama');
 console.log('Looping Kedua');
   for (kedua = 20; kedua >= 2; kedua = kedua -2){   
     console.log(kedua+ " - " + ' will become a mobile developer');
-    }  
+    } 
+console.log("=====================================")
+var i;
+for(i = 1; i <= 20; i++){
+  if(i % 2 == 0){
+    console.log(i + ' - Berkualitas');
+  } if(i % 2 == 1){
+   if(i % 3 == 0){
+    console.log(i + ' - I Love Ngoding'); 
+  }
+  else {
+    console.log(i + ' - Santai');
+  }
+}
+}
