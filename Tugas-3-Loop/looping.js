f7d1b767f97de426728254d76ae258e48559bc63
tmikma <tmikmal@gmail.com>
2021-01-13 12:44:41 +0700

    looping tugas 3

---
+++ Tugas-3-Loop/looping.js
@@ -0,0 +1,10 @@
+var pertama;
+var kedua;
+console.log('Looping Pertama');
+  for (pertama = 2; pertama <= 20; pertama = pertama +2){   
+    console.log(pertama+ " - " + 'I love coding');
+    }  
+console.log('Looping Kedua');
+  for (kedua = 20; kedua >= 2; kedua = kedua -2){   
+    console.log(kedua+ " - " + ' will become a mobile developer');
+    }  
