cf80e077b3106bd8326865c594494e8dd857be12
tmikma <tmikmal@gmail.com>
2021-01-14 16:37:45 +0700

    jawaban Tugas No. 4

---
+++ Tugas-4-Function/function.js
@@ -0,0 +1,27 @@
+console.log("=====================================")
+function teriak() {
+    console.log("halo Sanbers!");
+  }
+   
+  teriak(); 
+console.log("=====================================")
+function kalikan(num1, num4) {
+    return num1 * num4
+  }
+   var num1 = 12
+   var num2 = 4
+   var hasilkali = kalikan(num1, num2)
+  console.log(hasilkali)
+console.log("=====================================")
+function introduce(name,age,address,hobby) {
+    return "Nama Saya " +name+ ", Umur Saya " +age+ ", Alamat saya di "  +address+ "dan saya punya Hobby yaitu" + hobby+"!"
+  }
+  var name = "Agus"
+  var age = 30
+  var address = "Jln. Malioboro, Yogyakarta"
+  var hobby = "Gaming"
+   
+  var perkenalan = introduce(name, age, address, hobby)
+  console.log(perkenalan)
+  console.log("=====================================")
+  
